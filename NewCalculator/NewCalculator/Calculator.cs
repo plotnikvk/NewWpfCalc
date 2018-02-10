2f77b61fdfbeffcd9c33ad4e344066e974ff1a56
plotnikvk <plotnikov.vova@mail.ru>
2018-02-10 11:07:25 +0300

    lastUpdates 10.02.2018

---
+++ NewCalculator/NewCalculator/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewCalculator
+{
+    class Calculator
+    {
+        private decimal firstNumberSave;
+        private decimal secondNumberSave;
+    }
+}
---
+++ NewCalculator/NewCalculator/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calc;
+        public MainWindow()
+        {
+            InitializeComponent();
+            calc = new Calculator();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+            string BtnTag = pressedButton.Tag.ToString();
+            outputLabel.Content += BtnTag;
+            actualLabel.Content = outputLabel.Content;
+        }
+    }
+}
